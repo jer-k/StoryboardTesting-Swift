ebcb67a02e922c59383fa0b79d66c3f93215878b
Jeremy Kreutzbender <jkreutzbender@truecar.com>
2014-08-27 17:26:49 -0700

    Initial Commit

---
+++ TestXibStoryBoard/TestViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TestViewController.swift
+//  TestXibStoryBoard
+//
+//  Created by Jeremy Kreutzbender on 8/27/14.
+//
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    
+    /* Comment this init method in to see that the xib is loaded correctly
+    required init(coder aDecoder: NSCoder) {
+        super.init(nibName: "TestViewController", bundle: NSBundle.mainBundle())
+    }
+    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
